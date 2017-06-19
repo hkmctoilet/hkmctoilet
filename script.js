0f27e402f18d4f74d099c680cebad0c43ee58f2a
lkj1197 <lkjljs1197@naver.com>
2017-06-19 16:37:36 +0900

    no message

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-	app.listen(process.env.PORT || 8080, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-	});
+	var express = require("express"), 
+	app = express(); // Heroku의 환경 변수 PORT가 존재하면 환경변수를 사용하고 , 없다면 5000포트를 사용하도록한다. 
+	// 로컬에서 실행하기 위해 설정한다. 
+	app.set("port" , (process.env.PORT || 5000)); 
+	app.listen(app.get("port") , 
+		function(){ 
+			console.log("APP IS RUNNING ON ["+ app.get("port") +"]");
+			}); 
+			app.get("/" , function(req , res){
+				res.send("Hello World!"); 
+			}); 
 
 
 	var myApp = angular.module("myApp",["ngRoute"]);
